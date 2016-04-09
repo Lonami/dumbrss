65b3cabd22f926e435245dbbbd716b668bd80809
Dan Elkouby <streetwalrus@codewalr.us>
2016-04-09 12:41:55 +0300

    Phase out user management

--- dumbrss.py
+++ dumbrss.py
@@ -7,7 +7,6 @@ import feedparser
 import flask.ext.script as script
 import flask.ext.sqlalchemy as f_sqlalchemy
 import sqlalchemy
-import flask.ext.login as flask_login
 import flask_wtf
 import wtforms
 import urllib.parse as urlparse
@@ -33,9 +32,6 @@ if app.config["SECRET_KEY"] == None:
 
 db = f_sqlalchemy.SQLAlchemy(app)
 manager = script.Manager(app)
-
-login_manager = flask_login.LoginManager(app)
-login_manager.login_view = "login"
 
 # Set the timezone to UTC for consistent time stamps
 os.environ["TZ"] = "UTC"
@@ -68,8 +64,6 @@ class Entry(db.Model):
 
 class Feed(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    owner_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    owner = db.relationship("User", backref = db.backref("feeds", lazy = "dynamic"))
     folder_id = db.Column(db.Integer, db.ForeignKey("folder.id"))
     folder = db.relationship("Folder", backref = db.backref("feeds", lazy = "dynamic"))
     name = db.Column(db.Text)
@@ -77,8 +71,7 @@ class Feed(db.Model):
     link = db.Column(db.Text)
     url = db.Column(db.Text)
 
-    def __init__(self, owner, folder, name, icon, link, url):
-        self.owner = owner
+    def __init__(self, folder, name, icon, link, url):
         self.folder = folder
         self.name = name
         self.icon = icon
@@ -112,54 +105,13 @@ class Feed(db.Model):
 
 class Folder(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    owner_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    owner = db.relationship("User", backref = db.backref("folders", lazy = "dynamic"))
     name = db.Column(db.Text)
 
-    def __init__(self, owner, name):
-        self.owner = owner
+    def __init__(self, name):
         self.name = name
 
     def __repr__():
         return "<Folder {0} ({1})>".format(self.id, self.name)
-
-class User(db.Model, flask_login.UserMixin):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.Text)
-    password = db.Column(db.Text)
-    salt = db.Column(db.Binary)
-    admin = db.Column(db.Integer)
-
-    def get_id(self):
-        return self.name
-
-    def __init__(self, name, password, admin):
-        self.name = name
-        self.salt = os.urandom(16)
-        self.password = flask_login.make_secure_token(password.encode(), self.salt)
-        self.admin = admin
-
-    def __repr__():
-        return "<User {0} ({1})>".format(self.id, self.name)
-
-class LoginForm(flask_wtf.Form):
-    import wtforms.validators as v
-    username = wtforms.StringField("Username",
-            validators = [ v.DataRequired("Please enter your username") ])
-    password = wtforms.PasswordField("Password",
-            validators = [ v.DataRequired("Please enter your password") ])
-    remember = wtforms.BooleanField("Remember me")
-
-    def validate_username(self, field):
-        if load_user(field.data) == None:
-            raise wtforms.validators.StopValidation("Invalid username")
-
-    def validate_password(form, field):
-        user = load_user(form.username.data)
-        if user != None:
-            password = flask_login.make_secure_token(field.data.encode(), user.salt)
-            if password != user.password:
-                raise wtforms.validators.StopValidation("Invalid password")
 
 class AddFeedForm(flask_wtf.Form):
     import wtforms.validators as v
@@ -184,13 +136,10 @@ def urlopen_mozilla(url):
 @app.route("/feed/<int:feed_id>")
 @app.route("/folder/<int:folder_id>")
 @app.route("/starred", defaults = { "starred": True })
-@flask_login.login_required
 def feedview(folder_id = None, feed_id = None, starred = False):
     a = flask.request.args.get("a")
     if a == "setread" or a == "setstarred":
         entry = Entry.query.get_or_404(flask.request.args.get("id") or 0)
-        if entry.feed.owner.id != flask_login.current_user.id:
-            flask.abort(401)
         try:
             f = int(flask.request.args.get("f"))
         except ValueError:
@@ -208,15 +157,11 @@ def feedview(folder_id = None, feed_id = None, starred = False):
 
     if feed_id:
         feed = Feed.query.get_or_404(feed_id)
-        if feed.owner_id != flask_login.current_user.id:
-            flask.abort(401)
         title = feed.name
         entries = entries.filter_by(feed_id = feed_id)
 
     elif folder_id:
         folder = Folder.query.get_or_404(folder_id)
-        if folder.owner_id != flask_login.current_user.id:
-            flask.abort(401)
         title = folder.name
         entries = entries.join("feed").filter_by(folder_id = folder_id)
 
@@ -227,7 +172,7 @@ def feedview(folder_id = None, feed_id = None, starred = False):
     else:
         title = "Home"
 
-    entries = entries.join("feed").filter_by(owner_id = flask_login.current_user.id)
+    entries = entries.join("feed")
 
     try:
         page = int(flask.request.args.get("p") or 1)
@@ -239,35 +184,7 @@ def feedview(folder_id = None, feed_id = None, starred = False):
 
     return flask.render_template("feedview.html", entries = entries, title = title,
             folder_id = folder_id, feed_id = feed_id, starred = starred,
-            addfeedform = addfeedform)
-
-@app.route("/login", methods = [ "GET", "POST" ])
-def login():
-    if flask_login.current_user.is_authenticated():
-        return flask.redirect("/")
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = load_user(form.username.data)
-        flask_login.login_user(user, remember = form.remember.data)
-        flask.flash("Welcome, " + flask_login.current_user.name + ".", "success")
-        next_page = flask.request.args.get("next")
-        if not(redirect_is_local(next_page)):
-            next_page = None
-        return flask.redirect(next_page or "/")
-    else:
-        flash_errors(form)
-    return flask.render_template("login.html", form = form)
-
-@app.route("/logout")
-@flask_login.login_required
-def logout():
-    flask_login.logout_user()
-    return flask.redirect("/login")
-
-@login_manager.user_loader
-def load_user(username):
-    return User.query.filter(sqlalchemy.func.lower(User.name) ==
-            sqlalchemy.func.lower(username)).first()
+            addfeedform = addfeedform, feeds = Feed.query, folders = Folder.query)
 
 def fetch_feeds():
     for feed in Feed.query.yield_per(1000):
@@ -278,7 +195,6 @@ def fetch_feed(id):
     f = Feed.query.filter_by(id = id).first().fetch()
 
 @app.route("/addfeed", methods = [ "POST" ])
-@flask_login.login_required
 def add_feed():
     form = AddFeedForm()
     if form.validate_on_submit():
@@ -288,7 +204,7 @@ def add_feed():
         if hasattr(f, "bozo_exception"):
             flask.flash("This is not a valid feed", "danger")
             return flask.redirect("/")
-        if flask_login.current_user.feeds.filter_by(url = url).count():
+        if Feed.query.filter_by(url = url).count():
             flask.flash("This feed already exists", "danger")
             return flask.redirect("/")
         page = BeautifulSoup(urlopen_mozilla(f.feed.link))
@@ -301,7 +217,7 @@ def add_feed():
                 urlopen_mozilla(icon)
             except urlerror.HTTPError:
                 icon = None
-        newfeed = Feed(flask_login.current_user, None, f.feed.title, icon, f.feed.link, url)
+        newfeed = Feed(None, f.feed.title, icon, f.feed.link, url)
         db.session.add(newfeed)
         db.session.commit()
         newfeed.fetch()
@@ -326,30 +242,6 @@ def fetch(id):
             fetch_feed(id)
         except AttributeError:
             print("No feed with ID", id)
-
-@manager.command
-def adduser():
-    "Add a user to the database"
-    username = None
-    password = None
-    admin = None
-
-    while username == None:
-        username = script.prompt("Username")
-
-    while password == None:
-        password = script.prompt_pass("Password")
-
-    while admin == None:
-        admin = script.prompt_bool("Admin")
-
-    if load_user(username) == None:
-        user = User(username, password, 1 if admin else 0)
-        db.session.add(user)
-        db.session.commit()
-        print("Successfully added " + username)
-    else:
-        print("This user already exists!")
 
 @manager.command
 def initdb():
